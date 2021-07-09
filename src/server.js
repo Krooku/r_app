25d27123b0abcd97737aff84fbf6e9931d8c78e5
Krooku <krokumeister@gmail.com>
2021-07-09 02:14:45 +0200

    deploy

--- src/server.js
+++ src/server.js
@@ -22,9 +22,9 @@ app.use(session({
       httpOnly: true
     }
   }))
-app.use(express.json())
+// app.use(express.json())
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({extended: true, limit: '50mb'}));
+// app.use(express.urlencoded({extended: true, limit: '50mb'}));
 app.use('/api', router)
 app.use('/static', express.static(path.join(__dirname, '/../public/')))
 app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, '/../public/index.html')))
